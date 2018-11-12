3c8532ed3a4a3b11add8b3d7c75cd54a53705432
Sergey <xxsmotur@gmail.com>
2018-11-12 18:38:27 +0500

    Add init for slack

---
+++ alerting/slack/slack.go
@@ -0,0 +1,14 @@
+package slack
+
+import (
+	"github.com/nlopes/slack"
+)
+
+// Slack defines sending of alerts via Slack
+type Email struct {
+	email string
+}
+
+func New() error {
+	return nil
+}
