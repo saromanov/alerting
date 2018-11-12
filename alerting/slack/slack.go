c60df6984a55e5ba9fac3651a79bafbfa9199a5e
Sergey <xxsmotur@gmail.com>
2018-11-12 18:43:23 +0500

    Draft for sending message to Slack

--- alerting/slack/slack.go
+++ alerting/slack/slack.go
@@ -1,14 +1,32 @@
 package slack
 
 import (
+	"fmt"
 	"github.com/nlopes/slack"
 )
 
 // Slack defines sending of alerts via Slack
-type Email struct {
-	email string
+type Slack struct {
+	token string
+	channelID string
+	api *slack.Api
 }
 
-func New() error {
+// New provides init of the Slack
+func New() *Slack {
+	api := slack.New("YOUR_TOKEN_HERE")
+	return &Slack{
+		api:api,
+	}
+}
+
+// Send provides sending message to Slack
+func (s *Slack) Send() error {
+	channelID, timestamp, err := s.PostMessage(s.channelID, slack.MsgOptionText("Some text", false), slack.MsgOptionAttachments(attachment))
+	if err != nil {
+		return fmt.Errorf("unable to send message to Slack: %v", err)
+	}
+	
 	return nil
 }
+
