107aa544e8e8d7ba1e81e4ac72e6b55a0868b3cb
Sergey <xxsmotur@gmail.com>
2018-11-14 01:31:39 +0500

    Add db interface

--- db/db.go
+++ db/db.go
@@ -1,1 +1,10 @@
 package db
+
+import "github.com/saromanov/alerting/structs"
+
+// DB defines
+type DB interface {
+	Set(m *structs.Message) error
+	Search() ([]*structs.Message, error)
+	Get(id string) (*structs.Message, error)
+}
