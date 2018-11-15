6a6a88213def132caa31c0a6061ea555b0f4e261
Sergey <xxsmotur@gmail.com>
2018-11-15 18:46:01 +0500

    Return message response after Send to Slack

--- alerting/slack/slack.go
+++ alerting/slack/slack.go
@@ -24,11 +24,14 @@ func New(token string) *Slack {
 }
 
 // Send provides sending message to Slack
-func (s *Slack) Send(m *structs.Message) error {
+func (s *Slack) Send(m *structs.Message) (*structs.MessageResponse, error) {
 	channelID, timestamp, err := s.PostMessage(s.channelID, slack.MsgOptionText(m.Text, false), nil)
 	if err != nil {
-		return fmt.Errorf("unable to send message to Slack: %v", err)
+		return nil, fmt.Errorf("unable to send message to Slack: %v", err)
 	}
 
-	return nil
+	return &structs.MessageResponse{
+		ChannelID: channelID,
+		Timestamp: timestamp,
+	}, nil
 }
