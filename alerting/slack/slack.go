ed163013b862183d6f71ce50f5ee18b426e0a6d6
Sergey <xxsmotur@gmail.com>
2018-11-15 18:01:59 +0500

    updating of slack method send

--- alerting/slack/slack.go
+++ alerting/slack/slack.go
@@ -2,32 +2,33 @@ package slack
 
 import (
 	"fmt"
+
 	"github.com/nlopes/slack"
+	"github.com/saromanov/alerting/structs"
 )
 
 // Slack defines sending of alerts via Slack
 type Slack struct {
-	token string
+	token     string
 	channelID string
-	api *slack.Api
+	api       *slack.Api
 }
 
 // New provides init of the Slack
 func New(token string) *Slack {
 	api := slack.New(token)
 	return &Slack{
-		api:api,
-		token:token,
+		api:   api,
+		token: token,
 	}
 }
 
 // Send provides sending message to Slack
-func (s *Slack) Send() error {
-	channelID, timestamp, err := s.PostMessage(s.channelID, slack.MsgOptionText("Some text", false), slack.MsgOptionAttachments(attachment))
+func (s *Slack) Send(m *structs.Message) error {
+	channelID, timestamp, err := s.PostMessage(s.channelID, slack.MsgOptionText(m.Text, false), nil)
 	if err != nil {
 		return fmt.Errorf("unable to send message to Slack: %v", err)
 	}
-	
+
 	return nil
 }
-
