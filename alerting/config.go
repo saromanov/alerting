536fb27ee95b4240cd94372624a33b53676e8792
Sergey <xxsmotur@gmail.com>
2018-11-16 21:16:04 +0500

    Addd efinition for Config

--- alerting/config.go
+++ alerting/config.go
@@ -2,4 +2,8 @@ package alerting
 
 // Config defines configuration for alerting
 type Config struct {
+	SlackToken    string `yaml:"slack_token"`
+	EmailHost     string `yaml:"email_host"`
+	EmailUserName string `yaml:"email_username"`
+	EmailPassword string `yaml:"email_password"`
 }
