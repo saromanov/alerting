aa88198fafd6fe633b43ffbe4304f8fdb8cec1ab
Sergey <xxsmotur@gmail.com>
2018-11-16 20:34:12 +0500

    updating response after send email

--- alerting/email/email.go
+++ alerting/email/email.go
@@ -2,6 +2,7 @@ package email
 
 import (
 	"fmt"
+
 	"github.com/go-gomail/gomail"
 	"github.com/saromanov/alerting/structs"
 )
@@ -17,17 +18,17 @@ func New() error {
 
 // Send provides sending of email
 func (e *Email) Send(m *structs.Message) (*structs.MessageResponse, error) {
-	m := gomail.NewMessage()
-	m.SetHeader("From", e.email)
-	m.SetHeader("To", e.email)
-	m.SetHeader("Subject", "Alert!")
-	m.SetBody("text/html", m.Text)
+	g := gomail.NewMessage()
+	g.SetHeader("From", e.email)
+	g.SetHeader("To", e.email)
+	g.SetHeader("Subject", "Alert!")
+	g.SetBody("text/html", m.Text)
 
 	d := gomail.NewDialer("smtp.example.com", 587, "user", "123456")
 
 	// Send the email to Bob, Cora and Dan.
-	if err := d.DialAndSend(m); err != nil {
-		return fmt.Errorf("unable to send email: %v", err)
+	if err := d.DialAndSend(g); err != nil {
+		return nil, fmt.Errorf("unable to send email: %v", err)
 	}
-	return nil
+	return &structs.MessageResponse{}, nil
 }
