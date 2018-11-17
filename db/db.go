753d853ee0f61170f4c50dc9fda9f0dc58aa0603
Sergey <xxsmotur@gmail.com>
2018-11-17 18:37:56 +0500

    Change of the method name at DB interface

--- db/db.go
+++ db/db.go
@@ -5,6 +5,6 @@ import "github.com/saromanov/alerting/structs"
 // DB defines
 type DB interface {
 	Set(m *structs.Message) error
-	Search() ([]*structs.Message, error)
+	View() ([]*structs.Message, error)
 	Get(id string) (*structs.Message, error)
 }
