b07a27819c470440e4507a87f0d49937999550b0
Sergey <xxsmotur@gmail.com>
2019-01-17 11:54:59 +0500

    Source: implementation of the Do method

--- source/file/file.go
+++ source/file/file.go
@@ -1,12 +1,15 @@
 package file
 
 import (
+	"fmt"
+
 	"github.com/hpcloud/tail"
 	"github.com/saromanov/alerting/source"
 )
 
 type client struct {
 	t *tail.Tail
+	c *source.Config
 }
 
 // New provides initialization of the file watcher method
@@ -18,5 +21,14 @@ func New(s *source.Config) (source.Source, error) {
 
 	return client{
 		t: t,
+		c: s,
 	}, nil
+}
+
+// Do provides handling of the log output
+func (c client) Do() error {
+	for line := range c.t.Lines {
+		fmt.Println(line.Text)
+	}
+	return nil
 }
--- source/source.go
+++ source/source.go
@@ -3,10 +3,10 @@ package source
 // Source defines interface for reading data from source
 // as stream
 type Source interface {
-	Get(*Config) error
+	Do() error
 }
 
 // Config defines configuration for specification of source
 type Config struct {
-	Path string 
+	Path string
 }
