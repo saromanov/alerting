8e9efd78d115620bca1e48b949c3ab2ca32f8880
Sergey <xxsmotur@gmail.com>
2019-01-17 11:52:37 +0500

    Fix typo

--- source/file/file.go
+++ source/file/file.go
@@ -1,21 +1,22 @@
 package file
 
 import (
-	"github.com/saromanov/alerting/source"
 	"github.com/hpcloud/tail"
+	"github.com/saromanov/alerting/source"
 )
+
 type client struct {
 	t *tail.Tail
 }
 
 // New provides initialization of the file watcher method
-func New(s *source.Config) (client, error){
+func New(s *source.Config) (source.Source, error) {
 	t, err := tail.TailFile("/var/log/nginx.log", tail.Config{Follow: true})
 	if err != nil {
 		return nil, err
 	}
 
 	return client{
-		t:t,
+		t: t,
 	}, nil
 }
