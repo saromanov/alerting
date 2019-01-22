e0ce15e38e4a4da23835493642fb38a9ddd13d87
Sergey <xxsmotur@gmail.com>
2019-01-23 01:59:07 +0500

    rename db to storage

--- storage/db.go
+++ storage/db.go
@@ -1,9 +1,9 @@
-package db
+package storage
 
 import "github.com/saromanov/alerting/structs"
 
-// DB defines
-type DB interface {
+// Storage defines interface for storage operations
+type Storage interface {
 	Set(m *structs.Message) error
 	View() ([]*structs.Message, error)
 	Get(id string) (*structs.Message, error)
