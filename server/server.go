79a91649afb007cbaef08da9100e20ef496b2d6d
Sergey <xxsmotur@gmail.com>
2019-01-29 11:28:18 +0500

    add more endpoints

--- server/server.go
+++ server/server.go
@@ -3,9 +3,24 @@ package server
 import (
 	"log"
 	"net/http"
+	"github.com/go-chi/chi"
 )
+
+// metrics provides output for metrics
+func metrics(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// config provides output for config
+func config(w http.ResponseWriter, r *http.Request) {
+
+}
+
 
 // Create provides initialization of the server
 func Create(addr string) {
-	log.Fatal(http.ListenAndServe(addr, nil))
+	r := chi.NewRouter()
+	r.Get("/v1/metrics", metrics)
+	r.Get("/v1/config", config)
+	log.Fatal(http.ListenAndServe(addr, r))
 }
