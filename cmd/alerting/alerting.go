c74cfde71b906520fe9f929da88f6d3e7eabe0e2
Sergey <xxsmotur@gmail.com>
2019-01-29 11:36:24 +0500

    Add flag for alerting config

--- cmd/alerting/alerting.go
+++ cmd/alerting/alerting.go
@@ -2,26 +2,33 @@ package alerting
 
 import (
 	"fmt"
-	"github.com/urfave/cli"
+
 	"github.com/saromanov/alerting/server"
+	"github.com/urfave/cli"
 )
 
 var flags = []cli.Flag{
 	cli.StringFlag{
 		EnvVar: "SERVER_ADDRESS",
-		Name:   "github-token",
-		Usage:  "using of github token for test",
+		Name:   "server-address",
+		Usage:  "setup address for alerting app",
+	},
+
+	cli.StringFlag{
+		EnvVar: "ALERTING_CONFIG",
+		Name:   "alerting-config",
+		Usage:  "path to alerting config",
 	},
 }
 
 // setupServer provides setup of the server
-func setupServer(c *cli.Context) (server*Server, error) {
+func setupServer(c *cli.Context) (*server.Server, error) {
 	s, err := server.Create(c.String("github-token"))
 	if err != nil {
 		return nil, fmt.Errorf("unable to setup server: %v", err)
 	}
 	return s, nil
 }
-func main(){
+func main() {
 
 }
