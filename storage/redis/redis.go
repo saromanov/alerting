acf491a62140024e75b90f93dcdef7643a8506d0
Sergey <xxsmotur@gmail.com>
2019-02-13 16:11:06 +0500

    setup of the redis storage

--- storage/redis/redis.go
+++ storage/redis/redis.go
@@ -1,1 +1,28 @@
 package redis
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+	"github.com/saromanov/alerting/storage"
+)
+
+// db provides handling of the redis
+type db struct {
+}
+
+// Setup provides initialization of Redis
+func Setup() (storage.Storage, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	_, err := client.Ping().Result()
+	if err != nil {
+		return nil, fmt.Errorf("unable to setup redis: %v", err)
+	}
+
+	return &db{}, nil
+}
